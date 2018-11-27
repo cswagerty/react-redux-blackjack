973cc1372afd4f6b72cca0f91cfae9d3e0380b91
Corbin Swagerty <corbin.swagerty@gmail.com>
2018-11-27 11:34:51 -0800

    Refactored tests

--- __tests__/components/login-form.test.js
+++ __tests__/components/login-form.test.js
@@ -3,26 +3,29 @@ import { render, cleanup, fireEvent } from 'react-testing-library';
 
 import LoginForm from '../../src/app/components/login-form.jsx';
 
-afterEach(cleanup);
+describe('LoginForm', () => {
 
-test('LoginForm should exist', () => {	
-	expect(LoginForm).toBeDefined();
-});
+	afterEach(cleanup);
 
-test('LoginForm should render a text form with a text input', () => {	
-	const { container } = render(<LoginForm />);
-	
-	const formEls = container.querySelectorAll('form');
-	expect(formEls.length).toBe(1);
+	it('should exist', () => {	
+		expect(LoginForm).toBeDefined();
+	});
 
-	const textInputEls = container.querySelectorAll('input[type="text"]');
-	expect(textInputEls.length).toBe(1);
-});
+	it('should render a text form with a text input', () => {	
+		const { container } = render(<LoginForm />);
+		
+		const formEls = container.querySelectorAll('form');
+		expect(formEls.length).toBe(1);
 
-test('LoginForm calls submit callback on submit', () => {
-	const handleLoginSubmit = jest.fn();
-	const { container } = render(<LoginForm handleLoginSubmit={handleLoginSubmit} />);
-	const loginSubmitEl = container.querySelector('input[type="submit"]')
-	fireEvent.click(loginSubmitEl);
-	expect(handleLoginSubmit).toHaveBeenCalled();
+		const textInputEls = container.querySelectorAll('input[type="text"]');
+		expect(textInputEls.length).toBe(1);
+	});
+
+	it('calls submit callback on submit', () => {
+		const handleLoginSubmit = jest.fn();
+		const { container } = render(<LoginForm handleLoginSubmit={handleLoginSubmit} />);
+		const loginSubmitEl = container.querySelector('input[type="submit"]')
+		fireEvent.click(loginSubmitEl);
+		expect(handleLoginSubmit).toHaveBeenCalled();
+	});
 });
